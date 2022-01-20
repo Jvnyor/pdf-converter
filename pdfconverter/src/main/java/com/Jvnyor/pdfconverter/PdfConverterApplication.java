07cfa94ad9903395c8007419e975713ad0e34071
Jvnyor <josias100junior@gmail.com>
2022-01-20 11:33:48 -0300

    Removes .pdf of txt file

--- pdfconverter/src/main/java/com/Jvnyor/pdfconverter/PdfConverterApplication.java
+++ pdfconverter/src/main/java/com/Jvnyor/pdfconverter/PdfConverterApplication.java
@@ -17,42 +17,40 @@ import io.github.jonathanlink.PDFLayoutTextStripper;
 public class PdfConverterApplication {
 
     public static void main(String[] args) throws IOException {
-        
-        	String string = null;
-        	Scanner in = new Scanner(System.in);
-            System.out.println("Insira o caminho do pdf");
-            String pdfPath = in.nextLine();
-            File file = new File(pdfPath);
-            String txtName = file.getName();
-            String txtPath = "C:\\pdfConverter\\txts\\"+txtName+".txt";
-            if (pdfPath == null) {
-            	System.out.println("Erro! Caminho para pdf inválido ou pdf inválido!");
-            }
-            try {
+
+        String string = null;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Insira o caminho do pdf");
+        String pdfPath = in.nextLine();
+        File file = new File(pdfPath);
+        String txtName = file.getName().replace(".pdf", "");
+        String txtPath = "C:\\pdfConverter\\txts\\"+txtName+".txt";
+        if (pdfPath == null) {
+            System.out.println("Erro! Caminho para pdf inválido ou pdf inválido!");
+        }
+        try {
             	
-                PDFParser pdfParser = new PDFParser(new RandomAccessFile(file, "r"));
-                pdfParser.parse();
-                PDDocument pdDocument = new PDDocument(pdfParser.getDocument());
-                PDFTextStripper pdfTextStripper = new PDFLayoutTextStripper();
-                string = pdfTextStripper.getText(pdDocument);
+            PDFParser pdfParser = new PDFParser(new RandomAccessFile(file, "r"));
+            pdfParser.parse();
+            PDDocument pdDocument = new PDDocument(pdfParser.getDocument());
+            PDFTextStripper pdfTextStripper = new PDFLayoutTextStripper();
+            string = pdfTextStripper.getText(pdDocument);
                 
-                PrintWriter out = new PrintWriter(new FileOutputStream(txtPath));
-                String lines[] = string.split("\\r?\\n");
-    	        for (String line : lines) {
-    	        	
-    	        	out.println(line);
-    	        	
+            PrintWriter out = new PrintWriter(new FileOutputStream(txtPath));
+            String lines[] = string.split("\\r?\\n");
+    	    for (String line : lines) {	
+    	        out.println(line);
     	        }
-    	        
-                out.flush();
-                out.close();
-                System.out.println("Conversão completa!");
+
+            out.flush();
+            out.close();
+            System.out.println("Conversão completa!");
                 
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            };
+         } catch (FileNotFoundException e) {
+            e.printStackTrace();
+         } catch (IOException e) {
+            e.printStackTrace();
+         };
             
     }
 
