703365ac7856bb297abf979d86ccad1d0670c297
Jvnyor <josias100junior@gmail.com>
2022-02-15 09:36:21 -0300

    Refactoring code project

--- src/main/java/com/Jvnyor/pdfconverter/PdfConverterApp.java
+++ src/main/java/com/Jvnyor/pdfconverter/PdfConverterApp.java
@@ -22,43 +22,87 @@ public class PdfConverterApp {
 	 */
 	
 	public static void main(String[] args) {
-    	
-        try (Scanner in = new Scanner(System.in)) {
-			System.out.println("Insert the path of PDF file saved:");
-			String pdfPath = in.nextLine();
-			byte[] pdfPathBytes = pdfPath.getBytes();
-			String pdfPathToUTF8 = new String(pdfPathBytes, StandardCharsets.UTF_8);
-			
-			File file = new File(pdfPathToUTF8);
-			System.out.println("Insert the path of TXT file to save:");
-			String txtPath = in.nextLine();
-			byte[] txtPathBytes = txtPath.getBytes();
-			String txtPathToUTF8 = new String(txtPathBytes, StandardCharsets.UTF_8);
-			PDFParser pdfParser = new PDFParser(new RandomAccessFile(file, "r"));
-			pdfParser.parse();
-			PDDocument pdDocument = new PDDocument(pdfParser.getDocument());
-			PDFTextStripper pdfTextStripper = new PDFLayoutTextStripper();
-			String string = pdfTextStripper.getText(pdDocument);
-			        
-			PrintWriter out = new PrintWriter(new FileOutputStream(txtPathToUTF8));
-			String lines[] = string.split("\\r?\\n");
-			for (String line : lines) {	
-				out.println(line);
-			}
-			    
-			out.flush();
-			out.close();
-			in.close();
-			pdDocument.close();
-			System.out.println("Conversion completed!");
-			System.out.println("TXT file created in "+txtPath);
+
+		System.out.println("\n"
+				+".---. .---. .---.    .--.  .--. .-..-..-..-. .--. .---. .-----. .--. .---. \r\n"
+				+ ": .; :: .  :: .--'  : .--': ,. :: `: :: :: :: .--': .; :`-. .-': .--': .; :\r\n"
+				+ ":  _.': :: :: `;    : :   : :: :: .` :: :: :: `;  :   .'  : :  : `;  :   .'\r\n"
+				+ ": :   : :; :: :     : :__ : :; :: :. :: `' ;: :__ : :.`.  : :  : :__ : :.`.\r\n"
+				+ ":_;   :___.':_;     `.__.'`.__.':_;:_; `.,' `.__.':_;:_;  :_;  `.__.':_;:_;"
+				+"\n");
+		
+	     try (Scanner scanner = new Scanner(System.in)) {
+	        	
+	        char nextLine;
+	        	
+	        do {
+		        	
+				System.out.println("Insert the PDF path:");
+					
+				String pdfPath = scanner.nextLine();
+					
+				byte[] pdfPathBytes = pdfPath.getBytes();
+					
+				String pdfPathToUTF8 = new String(pdfPathBytes, StandardCharsets.UTF_8);
+					
+				File file = new File(pdfPathToUTF8);
+				
+				System.out.println("Insert the txt path to save:");
+					
+				String txtPath = scanner.nextLine();
+					
+				byte[] txtPathBytes = txtPath.getBytes();
+					
+				String txtPathToUTF8 = new String(txtPathBytes, StandardCharsets.UTF_8);
+					
+				PDFParser pdfParser = new PDFParser(new RandomAccessFile(file, "r"));
+					
+				pdfParser.parse();
+					
+				PDDocument pdDocument = new PDDocument(pdfParser.getDocument());
+					
+				PDFTextStripper pdfTextStripper = new PDFLayoutTextStripper();
+					
+				String string = pdfTextStripper.getText(pdDocument);
+					        
+				PrintWriter out = new PrintWriter(new FileOutputStream(txtPathToUTF8));
+					
+				String lines[] = string.split("\\r?\\n");
+					
+				for (String line : lines) {	
+						
+					out.println(line);
+						
+				}
+					    
+				out.flush();
+					
+				out.close();
+					
+				pdDocument.close();
+					
+				System.out.println("File created in "+txtPath);
+					
+				System.out.println("Do you wants new conversion? Y/N");
+					
+				nextLine = scanner.nextLine().charAt(0);
+					
+	        } while (nextLine == 'Y' || nextLine == 'y');
+	        	
+	        System.out.println("End program.");
+	        	
+	        scanner.close();
+	        	
 		} catch (FileNotFoundException e) {
+				
 			e.printStackTrace();
+				
 		} catch (IOException e) {
+				
 			e.printStackTrace();
-		} finally {
-			System.out.println("Program finished.");
+				
 		}
+		
 	}
-            
+
 }
