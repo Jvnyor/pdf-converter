a846f49da8ca4e65f1079fbe930b08b64ff50064
Jvnyor <josias100junior@gmail.com>
2022-04-12 09:42:19 -0300

    Adjusts

--- src/main/java/com/Jvnyor/pdfconverter/PdfConverterApp.java
+++ src/main/java/com/Jvnyor/pdfconverter/PdfConverterApp.java
@@ -15,11 +15,11 @@ import org.apache.pdfbox.text.PDFTextStripper;
 import io.github.jonathanlink.PDFLayoutTextStripper;
 
 public class PdfConverterApp {
-	
+
 	/**
 	 * @author Josias Junior https://github.com/Jvnyor
 	 */
-	
+
 	public static void main(String[] args) {
 
 		System.out.println("\n"
@@ -28,90 +28,87 @@ public class PdfConverterApp {
 				+ ":  _.': :: :: `;    : :   : :: :: .` :: :: :: `;  :   .'  : :  : `;  :   .'\r\n"
 				+ ": :   : :; :: :     : :__ : :; :: :. :: `' ;: :__ : :.`.  : :  : :__ : :.`.\r\n"
 				+ ":_;   :___.':_;     `.__.'`.__.':_;:_; `.,' `.__.':_;:_;  :_;  `.__.':_;:_;");
-		
+
 		char nextLine = 0;
-		
-		Scanner scanner = new Scanner(System.in);
-		
+
 		do {
-			
-			try {
-			        	
+
+			try (Scanner scanner = new Scanner(System.in)) {
+
 				String pdfPath = null;
-						
+
 				do {
-					
+
 					System.out.print("\nInsert the .pdf file path in computer: ");
-					
+
 					pdfPath = scanner.nextLine();
-					
-				} while (!pdfPath.contains(".pdf") || pdfPath == null || pdfPath.isBlank());
-				
+
+				} while (!pdfPath.contains(".pdf") || pdfPath == null || pdfPath.isBlank() || pdfPath.isEmpty());
+
 				File file = new File(pdfPath);
-					
+
 				String txtPath = null;
-						
+
 				do {
-					
+
 					System.out.print("\nInsert the .txt file path to save into computer: ");
-					
+
 					txtPath = scanner.nextLine();
-					
-				} while (!txtPath.contains(".pdf") || txtPath == null || txtPath.isBlank());
-				
+
+				} while (!txtPath.contains(".txt") || txtPath == null || txtPath.isBlank() || txtPath.isEmpty());
+
 				PDFParser pdfParser = new PDFParser(new RandomAccessFile(file, "r"));
-						
+
 				pdfParser.parse();
-						
+
 				PDDocument pdDocument = new PDDocument(pdfParser.getDocument());
-						
+
 				PDFTextStripper pdfTextStripper = new PDFLayoutTextStripper();
-						
+
 				String string = pdfTextStripper.getText(pdDocument);
-						        
+
 				PrintWriter out = new PrintWriter(new FileOutputStream(txtPath));
-						
+
 				String lines[] = string.split("\\r?\\n");
-						
-				for (String line : lines) {	
-							
+
+				for (String line : lines) {
+
 					out.println(line);
-							
+
 				}
-						
+
 				if (out != null) {
-					
-					System.out.println("\nFile created in "+txtPath);
-					
+
+					System.out.println("\nFile created in " + txtPath);
+
 				}
-						
+
 				out.flush();
-				
+
 				out.close();
-						
+
 				pdDocument.close();
-				
+
 				System.out.print("\nDo you wants new conversion? Y/N or any key: ");
-						
+
 				nextLine = scanner.nextLine().charAt(0);
-						
-		    } catch (FileNotFoundException e) {
-					
+
+			} catch (FileNotFoundException e) {
+
 				e.printStackTrace();
-					
+
 			} catch (IOException e) {
-					
+
 				e.printStackTrace();
-					
+
 			}
-		     
+
 			if (nextLine == 'N' || nextLine == 'n') {
-				scanner.close();
 				break;
 			}
-			
-	    } while (nextLine == 'Y' || nextLine == 'y');
-		
+
+		} while (nextLine == 'Y' || nextLine == 'y');
+
 	}
 
 }
